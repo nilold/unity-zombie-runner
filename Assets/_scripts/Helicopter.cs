5cb320451e6a71a86b410fffdf5759ee46da7ebb
nilo <nilold@gmail.com>
2018-10-20 15:11:20 -0300

    helicopter

--- Assets/_scripts/Helicopter.cs
+++ Assets/_scripts/Helicopter.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Helicopter : MonoBehaviour {
 
+    private bool called = true;
+    private AudioSource audioSource;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        CheckCallHelicopter();
 	}
+
+    private void CheckCallHelicopter()
+    {
+        if (Input.GetButton("Helicopter"))
+        {
+            called = true;
+            audioSource.Play();
+        }
+    }
 }
