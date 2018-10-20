338fcbe641f1bf984691a154a217772627b01c55
nilo <nilold@gmail.com>
2018-10-20 12:40:06 -0300

    created helicopter

---
+++ Assets/_scripts/Eyes.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+
+    private Camera eyes;
+    private float defaultFOV;
+    private bool isZoomed;
+    [SerializeField] float zoomLevel = 1.5f;
+
+
+	void Start () {
+        eyes = GetComponent<Camera>();
+        defaultFOV = eyes.fieldOfView;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckZoom();
+    }
+
+    private void CheckZoom()
+    {
+        if (Input.GetButton("Zoom"))
+        {
+            ZoomIn();
+        }
+        else
+        {
+            ZoomOut();
+        }
+    }
+
+    private void ZoomIn(){
+        if(!isZoomed){
+            eyes.fieldOfView = defaultFOV / zoomLevel;
+            isZoomed = true;
+        }
+
+    }
+
+    private void ZoomOut(){
+        if(isZoomed){
+            eyes.fieldOfView = defaultFOV;
+            isZoomed = false;
+        }
+            
+    }
+
+}
---
+++ Assets/_scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] GameObject spawnPoints;
+    [SerializeField] public bool respawn = false;
+    private Transform[] spawnPointsList;
+
+    private AudioSource audioSource;
+
+    [SerializeField] AudioClip callHelipterSound;
+
+    private void ReSpawn(){
+        int index = Random.Range(0, spawnPointsList.Length); //max value is exclusive
+        gameObject.transform.position = spawnPointsList[index].position;
+    }
+
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        spawnPointsList = spawnPoints.GetComponentsInChildren<Transform>();
+        ReSpawn();
+	}
+	
+	void Update ()
+    {
+        CheckReSpawn();
+        if(Input.GetButton("Helicopter")){
+            audioSource.PlayOneShot(callHelipterSound);
+        }
+    }
+
+    private void CheckReSpawn()
+    {
+        if (respawn)
+        {
+            ReSpawn();
+            respawn = false;
+        }
+    }
+}
